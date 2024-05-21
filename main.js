ef9aed67b7b2b0595280aa3c890ee5e9237e93cf
shukriddinovs <sshukriddinov524@gmail.com>
2024-05-21 14:13:28 +0500

    {.}

---
+++ main.js
@@ -0,0 +1,61 @@
+let a = prompt("1 dan 7gacha son kiriting")
+switch(a){
+case'1':
+alert('Dushanba')
+break
+case'2':
+alert('Seshanba')
+break
+case'3':
+alert('CHorshanba')
+break
+case'4':
+alert('Payshanba')
+break
+case'5':
+alert('juma')
+break
+case'6':
+alert('shanba')
+break
+case'7':
+alert('Yakshanba')
+break;
+default : alert("haftada 7 kun bor")
+
+}
+
+// let a = prompt(" Sizning tezligingiz ")
+
+// if (a < 60) {
+//     alert("belgilangan tezlik")
+// } else if (a > 60 && a < 66) {
+//     alert("siz belgilangan tezlikdan oshib ketingiz jarima yaqin")
+// } if (a > 67 && a < 90) {
+//     alert("sizga 500 ming som jarima")
+// } else if (a > 91 && a < 101) {
+//     alert("moshinangiz 2 kunga olinib qoladi")
+// }
+
+
+
+// switch (radar) {
+//     case '60 dan past bolsa':
+//         alert(' bugun havo yahshi ')
+//         break
+//     case 'yomgir':
+//         alert('soyabon oling')
+//         break
+//     case 'qor':
+//         alert('qaliroq kiyining')
+//         break
+//     case 'bulut':
+//         alert('yomgir yogish ehtimoli bor')
+//         break
+//     case 'dol':
+//         alert('uyingizdan chiqmang')
+//         break
+
+
+//     default: alert("bunday havo yoq")
+// }
